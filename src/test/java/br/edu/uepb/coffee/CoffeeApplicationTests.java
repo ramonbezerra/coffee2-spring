2f51369ad38b98ca290aa7e124577fbfb40ffbdc
Ramon Bezerra da Nóbrega <57471737+ramonbezerra@users.noreply.github.com>
2022-10-15 22:32:29 -0300

    include default test

--- src/test/java/br/edu/uepb/coffee/CoffeeApplicationTests.java
+++ src/test/java/br/edu/uepb/coffee/CoffeeApplicationTests.java
@@ -1,5 +1,7 @@
 package br.edu.uepb.coffee;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,7 @@ class CoffeeApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue(true);
 	}
 
 }
